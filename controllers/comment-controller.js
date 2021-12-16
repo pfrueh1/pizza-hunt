e6ff618f7b3fbe694ad3fd8e51710edfe59ec668
pfrueh1 <fruehpaul@gmail.com>
2021-12-16 14:24:16 -0500

    validation

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -25,7 +25,8 @@ const commentController = {
     Comment.findOneAndUpdate(
       { _id: params.commentId },
       { $push: { replies: body } },
-      { new: true }
+      { new: true, runValidators: true },
+
     )
       .then(dbPizzaData => {
         if (!dbPizzaData) {
